c7d45b791fedea836be1e4c5c357b1c6e80fd05b
634026752@qq.com <634026752@qq.com>
2020-12-22 00:25:36 +0800

    创建websocket

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -5,6 +5,9 @@
     <div class="main-container">
       <div :class="{'fixed-header':fixedHeader}">
         <navbar />
+        <div>
+          12312321312
+        </div>
       </div>
       <app-main />
     </div>
@@ -21,6 +24,11 @@ export default {
     Navbar,
     Sidebar,
     AppMain
+  },
+  data(){
+    return{
+       websock: null,
+    }
   },
   mixins: [ResizeMixin],
   computed: {
@@ -42,10 +50,41 @@ export default {
       }
     }
   },
+   created() {
+      this.initWebSocket();
+    },
+  destroyed(){
+     this.websock.close() //离开路由之后断开websocket连接
+  },
   methods: {
     handleClickOutside() {
       this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
-    }
+    },
+      initWebSocket(){ //初始化weosocket
+        const wsuri = "ws://192.168.3.19:8080/test/one";
+        this.websock = new WebSocket(wsuri);
+        this.websock.onmessage = this.websocketonmessage;
+        this.websock.onopen = this.websocketonopen;
+        this.websock.onerror = this.websocketonerror;
+        this.websock.onclose = this.websocketclose;
+      },websocketonopen(){ //连接建立之后执行send方法发送数据
+        let actions = {"test":"12345"};
+        this.websocketsend(JSON.stringify(actions));
+      },
+      websocketonerror(){//连接建立失败重连
+        this.initWebSocket();
+      },
+      websocketonmessage(e){ //数据接收
+        console.log(e.data)
+        const redata = JSON.parse(e.data);
+      
+      },
+      websocketsend(Data){//数据发送
+        this.websock.send(Data);
+      },
+      websocketclose(e){  //关闭
+        console.log('断开连接',e);
+      },
   }
 }
 </script>
